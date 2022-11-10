4ddb27cb573fe11cf916b09ca4fc0c560856823c
juki95 <oanhpham99x@gmail.com>
2022-11-10 19:47:54 +0700

    initial project

---
+++ src/main/java/com/athena/shopeejb101/ShopeeJb101Application.java
@@ -0,0 +1,13 @@
+package com.athena.shopeejb101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopeeJb101Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopeeJb101Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/athena/shopeejb101/ShopeeJb101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.athena.shopeejb101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopeeJb101ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
